d732e2e8b0390d2286c3cdb03e0089f65f14ec6b
Jason L West <jlwest@basspro.com>
2017-06-14 13:34:09 -0500

    Part 4 Checkin

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check DB connection
+db.once('open', () => {
+  console.log('Connected to MongoDB...');
+});
+
+// Check for DB errors
+db.on('error', (err) => {
+  console.log(err);
+});
 
 // Init App
 const app = express();
+
+// Bring in models
+let Article = require('./models/Article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,29 +27,15 @@ app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', (req, res) => {
-  let articles = [
-    {
-      id: 1,
-      title: 'Article One',
-      author: 'Jason L. West',
-      body: 'This is article one.'
-    },
-    {
-      id: 2,
-      title: 'Article Two',
-      author: 'John Doe',
-      body: 'This is article two.'
-    },
-    {
-      id: 3,
-      title: 'Article Three',
-      author: 'Jason L. West',
-      body: 'This is article three.'
-    },
-  ];
-  res.render('index', {
-    title: "Articles",
-    articles: articles,
+  Article.find({}, (err, articles) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: "Articles",
+        articles: articles,
+      });
+    }
   });
 });
 
